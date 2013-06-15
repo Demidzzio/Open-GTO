3e9ab3fcd74ed5766e0161efe65ce96065ebad2b
Sergey Marochkin <xziggix@gmail.com>
2013-06-15 20:19:07 +0400

    Open-GTO The Next Generation: start development

--- !compile.bat
+++
@@ -1,5 +0,0 @@
-set name=Open-GTO
-compiler\pawncc.exe -;+ -(+ sources\%name%.pwn
-if exist %name%.amx ^
-move %name%.amx gamemodes\
-pause
---
+++ sources/config.h
@@ -0,0 +1,59 @@
+/*
+	
+	About: defines and default settings
+	Author: ziggi
+
+*/
+
+#if defined _config_header_included
+	#endinput
+#endif
+
+#define _config_header_included
+#pragma library config_header
+
+/*
+	System
+*/
+#define GTO_VERSION				"2.0"
+#define GTO_FILES_FORMAT		".ini"
+#define GTO_FILES_FOLDER		"open-gto/"
+#define GTO_TEXT				"Open-GTO v"#GTO_VERSION
+
+#define LANG_USE				"russian"
+
+#define FILE_CONFIG				(GTO_FILES_FOLDER "config"GTO_FILES_FORMAT)
+#define ini_fileExist			fexist
+
+/*
+	Log
+*/
+#define LOG_PRINTING
+#define LOG_GAME_ENABLED		1
+#define LOG_USER_ENABLED		1
+#define LOG_ERROR_ENABLED		1
+#define LOG_DEBUG_ENABLED		1
+
+/*
+	Weather
+*/
+#define WEATHER_UPDATE_TIME		20
+
+/*
+	Skins
+*/
+#define SKINS_COST			0
+#define SKINS_IGNORE		0, 74
+
+/*
+	Mapicon streamer
+*/
+#define MAX_ICONS					1000
+#define MAX_SHOWED_ICONS			100 // sa-mp limit
+#define MAP_ICON_SHOW_DIST			400
+
+/*
+	Checkpoint streamer
+*/
+/*#define MAX_CP			255
+#define CP_SHOW_DIST	65000.0*/
